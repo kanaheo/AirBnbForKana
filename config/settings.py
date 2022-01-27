347f45e448feac015e89e5a8a590f2829d5b819b
heokyoungmin <stalvede0421@gmail.com>
2022-01-27 10:40:28 +0900

    chapter5 - reviews model setting

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ PROJECT_APPS = [
     "core.apps.CoreConfig",
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
+    "reviews.apps.ReviewsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Review(core_models.AbstractTimeStampedModel):
+
+    """Review Model Definition"""
+
+    review = models.TextField()
+    accuracy = models.IntegerField()  # 정확성
+    communication = models.IntegerField()  # 의사소통
+    cleanliness = models.IntegerField()  # 청결도
+    Location = models.IntegerField()  # 위치
+    check_in = models.IntegerField()  # 체크인
+    value = models.IntegerField()  # 가격 대비 만족도
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE
+    )  # 유저를 삭제하면 관련 리뷰도 삭제 !
+    room = models.ForeignKey(
+        "rooms.Room", on_delete=models.CASCADE
+    )  # 방을 삭제해도 관련 리뷰 삭제 !
+
+    def __str__(self):
+        return f"{self.review} - {self.room}"
