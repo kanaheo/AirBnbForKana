3be3204a1040919a4bf23017994e59c7a6a15e3d
heokyoungmin <stalvede0421@gmail.com>
2022-01-27 11:21:14 +0900

    chapter5 list setting end

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ PROJECT_APPS = [
     "rooms.apps.RoomsConfig",
     "reviews.apps.ReviewsConfig",
     "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
