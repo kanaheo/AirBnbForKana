f02fa9cd9e5fba333b4b297d51d2780ac888c495
heokyoungmin <stalvede0421@gmail.com>
2022-02-08 23:08:05 +0900

    수동으로 페이징

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
@@ -6,6 +7,22 @@ from . import models
 # 그리고 항상 반환해야함 ! httpResponse를 반환해야함 !
 # 우린 HttpResponse안에다 매번 이렇게 안에다가 <div>hello</div> 이런식으로 안하고 render을 쓸거다
 def all_rooms(request):  # <- 여기 이름은 core url부분에서 이름이 같아야!
-    all_rooms = models.Room.objects.all()
+    page = request.GET.get("page", 1)
+    page = int(page or 1)  # page가 공백일경우 page= <-이런거
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
+    page_range = range(1, page_count + 1)
 
-    return render(request, "rooms/index.html", context={"rooms": all_rooms})
+    return render(
+        request,
+        "rooms/index.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": page_range,
+        },
+    )
