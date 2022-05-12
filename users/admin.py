3fba4fba3509c7bcfc40656ef38554cbad923056
heokyoungmin <stalvede0421@gmail.com>
2022-05-12 12:23:50 +0900

    user app end

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
-    pass
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "langauge",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -7,18 +7,32 @@ class User(AbstractUser):
 
     """Custom User Model"""
 
-    GENDER_MALE = "male"
+    GENDER_MALE = "male"  # db에서 보여지는 값
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
 
     GENDER_CHOICES = (
-        (GENDER_MALE, "Male"),
+        (GENDER_MALE, "Male"),  # admin패널에서 보여지는 값
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
+
+    LANGUAGE_ENGLISH = "english"
+    LANGUAGE_KOREAN = "korean"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICES, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    langauge = models.CharField(choices=LANGUAGE_CHOICES, max_length=10, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
