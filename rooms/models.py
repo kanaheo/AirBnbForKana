26d3434594b385ef862a078b2b4bd027ead6d829
heokyoungmin <stalvede0421@gmail.com>
2022-06-07 19:04:21 +0900

    reviews seend data create & rooms edit

--- rooms/models.py
+++ rooms/models.py
@@ -103,6 +103,6 @@ class Room(core_models.TimeStampedModel):
         if len(all_reviews) > 0:
             for review in all_reviews:
                 all_ratings += review.rating_average()
-            return all_ratings / len(all_reviews)
+            return round(all_ratings / len(all_reviews), 2)
         else:
             return 0
