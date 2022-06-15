c9b5e65d6f1d8460fa3587899a2372bcf6a1ccdb
heokyoungmin <stalvede0421@gmail.com>
2022-06-16 00:01:10 +0900

    ch10 end

--- reservations/management/commands/seed_reservations.py
+++ reservations/management/commands/seed_reservations.py
@@ -1,8 +1,6 @@
 import random
 from datetime import datetime, timedelta
-from secrets import choice
 from django.core.management.base import BaseCommand
-from django.contrib.admin.utils import flatten
 from django_seed import Seed
 from reservations import models as reservation_models
 from users import models as user_models
