d89340f83185004d316e85c832dc03464975984b
heokyoungmin <stalvede0421@gmail.com>
2022-06-16 16:36:54 +0900

    ch11 end use django

--- rooms/views.py
+++ rooms/views.py
@@ -1,23 +1,12 @@
-from math import ceil
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = (
-        models.Room.objects.all()
-    )  # 여기서 불러 올 때는 그냥 선언만 ! 아직 디비에서 안가지고옴 ! 근데 저게 선언되면 디비를 불러옴
-    paginator = Paginator(room_list, 10)
-    try:
-        rooms = paginator.page(int(page))
-        return render(
-            request,
-            "rooms/home.html",
-            context={"page": rooms},
-        )
-    # except EmptyPage:
-    except Exception:   # 모든 에러
-        rooms = paginator.page(1)
-        return redirect("/")
+class HomeView(ListView):
+
+    """HOmeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    context_object_name = "rooms"
