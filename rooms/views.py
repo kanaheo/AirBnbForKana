984f90c91a84fd6edfc834f24162cb283ac82fe3
heokyoungmin <stalvede0421@gmail.com>
2022-06-16 12:20:32 +0900

    little django help

--- rooms/views.py
+++ rooms/views.py
@@ -1,24 +1,23 @@
 from math import ceil
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),
-        },
-    )
+    page = request.GET.get("page", 1)
+    room_list = (
+        models.Room.objects.all()
+    )  # 여기서 불러 올 때는 그냥 선언만 ! 아직 디비에서 안가지고옴 ! 근데 저게 선언되면 디비를 불러옴
+    paginator = Paginator(room_list, 10)
+    try:
+        rooms = paginator.page(int(page))
+        return render(
+            request,
+            "rooms/home.html",
+            context={"page": rooms},
+        )
+    # except EmptyPage:
+    except Exception:   # 모든 에러
+        rooms = paginator.page(1)
+        return redirect("/")
