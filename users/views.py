4592c3106ffedadb48846db5be44ab40bfc9273d
heokyoungmin <stalvede0421@gmail.com>
2022-06-23 00:19:03 +0900

    setting & csrf editing

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
+from django.views import View
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        form = forms.LoginForm()
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        print(form)
