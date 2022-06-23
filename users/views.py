d205cdf50c64add15c34501862fdbe8d637256ef
heokyoungmin <stalvede0421@gmail.com>
2022-06-23 11:17:00 +0900

    email & password check

--- users/views.py
+++ users/views.py
@@ -5,9 +5,13 @@ from . import forms
 
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm()
+        # form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "aaa@aaa.com"})  # test 때문에 기본 값 설정
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+        if form.is_valid():
+            print(form.cleaned_data)
+
+        return render(request, "users/login.html", {"form": form})
