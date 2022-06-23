101be1dd5669c315cc3279437dcdacdd77cb6a15
heokyoungmin <stalvede0421@gmail.com>
2022-06-23 11:40:55 +0900

    login & logout hard ways end

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 app_name = "users"
 
-urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
+urlpatterns = [
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
+    # MUST 아래처럼 쓰고 싶다면 views log_out함수가 클래스 밖에 있어야함 !
+    # path("logout", views.log_out, name="logout"),
+    # MUST 아래처럼 쓰고 싶다면 views log_out함수가 클래스 안에 있어야함 !
+    # path("logout", views.LoginView.log_out, name="logout"),
+]
