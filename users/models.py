32aacc6f98f3425e4b01e27ef122a66c952c1002
heokyoungmin <stalvede0421@gmail.com>
2022-06-30 11:09:39 +0900

    ch16 end

--- users/models.py
+++ users/models.py
@@ -61,5 +61,6 @@ class User(AbstractUser):
                 fail_silently=False,
                 html_message=html_message,
             )
+            self.save()
 
         return
--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,9 @@ app_name = "users"
 
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
-    path("logout", views.log_out, name="logout"),
-    path("signup", views.SignUpView.as_view(), name="signup"),
+    path("logout", views.log_out, name="logout"),  # 함수는 그냥
+    path("signup", views.SignUpView.as_view(), name="signup"),  # class 는 as_view
+    path("verify/<str:key>", views.complete_verification, name="complete-verification"),
     # MUST 아래처럼 쓰고 싶다면 views log_out함수가 클래스 밖에 있어야함 !
     # path("logout", views.log_out, name="logout"),
     # MUST 아래처럼 쓰고 싶다면 views log_out함수가 클래스 안에 있어야함 !
